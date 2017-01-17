c622de886d471fa91442c49e9f654f436d04fb9c
pronin <pronin.idenis@gmail.com>
2017-01-17 14:58:52 +0700

    fix components

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -7,22 +7,16 @@ export default class Counter extends React.Component {
     onIncrement: PropTypes.func.isRequired
   };
 
-  constructor(props) {
-    super(props);
-    this.onIncrement = this.props.onIncrement.bind(this);
-    this.onDecrement = this.props.onDecrement.bind(this);
-  }
-
   render () {
-    const { count } = this.props;
+    const { count, onDecrement, onIncrement } = this.props;
 
     return (
       <div className="counter">
         <h1>Count: {count}</h1>
-        <button type="button" onClick={this.onDecrement}>
+        <button type="button" onClick={onDecrement}>
           Decrement
         </button>
-        <button type="button" onClick={this.onIncrement}>
+        <button type="button" onClick={onIncrement}>
           Increment
         </button>
       </div>
